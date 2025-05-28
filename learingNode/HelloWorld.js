b33d733770570d187f7302cbbc71d5b7eb8ab285
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-05-28 15:34:32 +0530

    modified js files

--- learingNode/HelloWorld.js
+++ learingNode/HelloWorld.js
@@ -1,1 +1,8 @@
+const {addFn, subFn} =require('./Module')
+
+console.log(add(2,5),sub(5,2))
+
 console.log("hey there js ")
+
+
+   
---
+++ learingNode/Module.js
@@ -0,0 +1,18 @@
+function add(a,b) {
+    return a+b  
+}
+
+
+function sub(a,b) {
+    return a-b  
+}
+
+
+const operation ={
+    addFn: add,
+    subFn:sub
+}
+exports = add1 = (a ,b)=>a+b
+exports = sub1 = (a ,b)=>a-b
+
+module.exports = operation
