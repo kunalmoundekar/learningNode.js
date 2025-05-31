5e650d2481fb6d13cc15befc0269f053a680f5e7
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-05-31 20:22:57 +0530

    Handling  URL

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const http = require("http")
 const fs =require("fs")
-
+const url =require("url")
 
 const myServer  = http.createServer((req,res)=>{
 
+    if (req.url ==="/favicon.ico")return res.end()
+
     const log = `${Date.now()}  ${req.url}: New req Received\n`
+
+    const myUrl=url.parse(req.url ,true)
+    console.log(myUrl)
 
     fs.appendFile('log.txt',log,(err,data)=>{
 
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/':res.end("Home page")
             break
-            case '/about':res.end("about page")
+            case '/about':
+                const username =  myUrl.query.myname
+                res.end(`Hii, ${username} `)
             break;
+            case '/search':
+                const search = myUrl.query.search_query
+                res.end("Here are your results for "+ search)
+                  break;
+
             default:
                 res.end("404 Not Found")
 
