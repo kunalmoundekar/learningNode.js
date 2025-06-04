c57f0d06c66f2e5aace8376f0a68ccbafe1322a3
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-06-04 16:26:35 +0530

    solve express module err

--- server/express.js
+++ server/express.js
@@ -1,33 +1,15 @@
-const express = require("express")
+const express = require("express");
+const app = express();
 
-const app = express()
 
 app.get('/', (req, res) => {
-    return res.send("Hello From home Page")
-})
- 
+    return res.send("Hello From home Page");
+});
 
-
-app.get("/about",(req,res)=>{
-    return res.send(`Hello ${req.query.name}  you are  ${req.query.age}`)
-})
-
-
-
-
-
-
-
-
-
-
-const myServer = http.createServer(app)
+app.get("/about", (req, res) => {
+    return res.send(`Hello ${req.query.name}  you are  ${req.query.age}`);
+});
 
 app.listen(7000, () => {
-    console.log("server started express !")
-})
-
-
-
-
-
+    console.log("server started express !");
+});
