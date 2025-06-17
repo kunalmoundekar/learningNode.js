0f945568be16f41b29bcbb3c844dfd3baf04cb5c
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-06-17 16:07:13 +0530

    grouping a route

--- project01/index.js
+++ project01/index.js
@@ -42,12 +42,21 @@ app.get('/api/users', (req, res) => {
 
 
 
-app.get('/api/users/:id', (req, res) => {
+app
+.route('/api/users/:id')
+.get('/api/users/:id', (req, res) => {
 
     const id = Number(req.params.id)
     const user = users.find((user)=> user.id === id)
    return res.json(user)
+}).patch((req,res)=>{
+     // TOOD : Edit the user with id
+    return res.json({status : 'Pending'})
+}).delete('/api/users/:id',(req,res)=>{
+    // TOOD : delete the user with id
+   return res.json({status : 'pending'})
 })
+
 
 
 app.post('/api/users',(req,res) => {
@@ -55,16 +64,9 @@ app.post('/api/users',(req,res) => {
 
 })
 
-app.patch('/api/users/:id',(req,res)=>{
-    // TOOD : Edit the user with id
-   return res.json({status : 'pending'})
 
-})
 
-app.delete('/api/users/:id',(req,res)=>{
-    // TOOD : delete the user with id
-   return res.json({status : 'pending'})
-})
+
 
 
 
