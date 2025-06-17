ef5735f5a74769ed8fd4b7cb5729ef21035d00b9
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-06-17 15:58:28 +0530

    create a REST api

--- project01/index.js
+++ project01/index.js
@@ -5,16 +5,80 @@ const app = express()
 
 const PORT = 4000
 
-app.listen(PORT ,()=>{
-    console.log("Hello world")
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my Express server!")
 })
 
-app.get('/users',(req,res)=>{
+
+
+// app.get('/api/users', (req, res) => {
+
+//     const html = `<li>
+//     ${users.map((users)=> users.first_name).join("")}
+//     </li>`
+//     res.send(html)
+
+// })
+
+
+app.get('/users', (req, res) => {
+
+    const html = `<ul> 
+     ${users.map((users) => `<li>${users.first_name}</li>`)} </ul>`
+    res.send(html)
+
+})
+
+//REST API
+
+// GET /api/users/:id
+
+
+app.get('/api/users', (req, res) => {
     return res.json(users)
 })
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to my Express server!")
+
+
+app.get('/api/users/:id', (req, res) => {
+
+    const id = Number(req.params.id)
+    const user = users.find((user)=> user.id === id)
+   return res.json(user)
 })
+
+
+app.post('/api/users',(req,res) => {
+   return res.json({status : 'pending'})
+
+})
+
+app.patch('/api/users/:id',(req,res)=>{
+    // TOOD : Edit the user with id
+   return res.json({status : 'pending'})
+
+})
+
+app.delete('/api/users/:id',(req,res)=>{
+    // TOOD : delete the user with id
+   return res.json({status : 'pending'})
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Hello world")
+})
+
+
 
 
