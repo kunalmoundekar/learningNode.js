f272f91eb3a7fc28e5e67ecab313943de2ba138b
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-08 16:16:11 +0530

    modified index.js

--- project01/index.js
+++ project01/index.js
@@ -7,20 +7,14 @@ const PORT = 4000
 
 
 
+
 app.get('/', (req, res) => {
     res.send("Welcome to my Express server!")
 })
 
 
 
-// app.get('/api/users', (req, res) => {
 
-//     const html = `<li>
-//     ${users.map((users)=> users.first_name).join("")}
-//     </li>`
-//     res.send(html)
-
-// })
 
 
 app.get('/users', (req, res) => {
@@ -41,41 +35,25 @@ app.get('/api/users', (req, res) => {
 })
 
 
-
+ 
 app
-.route('/api/users/:id')
-.get('/api/users/:id', (req, res) => {
+    .route('/api/users/:id')
+    .get('/api/users/:id', (req, res) => {
+        const id = Number(req.params.id)
+        const user = users.find((user) => user.id === id)
+        return res.json(user)
+    }).patch((req, res) => {
+        // TOOD : Edit the user with id
+        return res.json({ status: 'Pending' })
+    }).delete('/api/users/:id', (req, res) => {
+        // TOOD : delete the user with id
+        return res.json({ status: 'pending' })
+    })
 
-    const id = Number(req.params.id)
-    const user = users.find((user)=> user.id === id)
-   return res.json(user)
-}).patch((req,res)=>{
-     // TOOD : Edit the user with id
-    return res.json({status : 'Pending'})
-}).delete('/api/users/:id',(req,res)=>{
-    // TOOD : delete the user with id
-   return res.json({status : 'pending'})
+
+app.post('/api/users', (req, res) => {
+    return res.json({ status: 'pending' })
 })
-
-
-
-app.post('/api/users',(req,res) => {
-   return res.json({status : 'pending'})
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log("Hello world")
