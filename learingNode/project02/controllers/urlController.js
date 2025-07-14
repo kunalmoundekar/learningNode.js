1b937a1a76d9dc8177aeae06f9f690b7ea707df5
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-14 20:50:35 +0530

    create a clean and easy-to-handle urlController

--- learingNode/project02/controllers/urlController.js
+++ learingNode/project02/controllers/urlController.js
@@ -1,31 +1,42 @@
-import express from 'express'
+// import express from 'express'
 import URL from "../models/urlModel.js";
 import shortid from 'shortid';
 
 
-
-
 export const GenerateShortUrl = async (req, res) => {
+    try {
 
+        const body = req.body
+        if (!body.url) {
+            return res.status(400).send({
+                success: false,
+                message: "url is required"
+            })
+        }
 
-    const body = req.body
-    if (!body.url) {
-        return res.status(400).send({
-            success: false,
-            message: "url is required"
+        const Id = shortid.generate();
+
+        const userUrl = await URL.create({
+            shortId: Id,
+            redirectURL: body.url,
+            visitHistory: []
+        })
+
+        return res.status(200).send({
+            success: true,
+            shortid: " got a userID",
+            userUrl
         })
     }
+    catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Server error",
+            error: error.message
+        })
 
-    const shortId = shortid.generate();
-    URL.create({
-        shortId: shortId,
-        redirectURL: body.url,
-        visitHistory: []
-    })
-
-    return res.status(200).send({
-        success: true,
-        shortid:shortid  
-    })
+    }
 
 }
+
