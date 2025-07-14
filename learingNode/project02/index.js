2227936570c13cf01e60b8ce1512f0a9acf67618
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-14 20:52:58 +0530

    create api server using index.js

--- learingNode/project02/index.js
+++ learingNode/project02/index.js
@@ -1,19 +1,49 @@
-
 import express from 'express';
+import dotenv from 'dotenv'
+import connectDB from './config/connectDB.js'
+import urlRoute from './routes/urlRoute.js'
+
+
+dotenv.config()
 
 
 
- import router from './routes/urlRoute.js';
- 
- 
- const app = express();
- const PORT = 5001;
-
- app.use(express.json());
+connectDB()
 
 //import routes
 
-app.use( '/url', router)
+const app = express();
+
+app.use(express.json());
+
+
+// use routes
+
+
+
+app.use(urlRoute)
+
+
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now()
+                }
+            }
+        })
+
+    res.redirect(entry.redirectURL)
+})
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is started at ${PORT}`);
