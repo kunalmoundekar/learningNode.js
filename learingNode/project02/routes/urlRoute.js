c04233f947dfbfce0d1f33fba43bb69e6b28ae9e
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-14 20:55:49 +0530

    create a urlRoutes

--- learingNode/project02/routes/urlRoute.js
+++ learingNode/project02/routes/urlRoute.js
@@ -1,11 +1,13 @@
 import express from 'express'
 
 import { GenerateShortUrl } from '../controllers/urlController.js';
+// import {UrlFeachController} from '../controllers/urlController.js'
 
-
+  
 const router = express.Router();
 
-router.post('/',GenerateShortUrl)
+router.post('/url',GenerateShortUrl)
+// router.get('/get',UrlFeachController)
 
 export default router
 
